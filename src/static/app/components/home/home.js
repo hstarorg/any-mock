31e0c71d4c5365091d4ccf1de63cada8f96e2dfd
Jay.M.Hu <hm910705@163.com>
2016-02-05 00:09:58 +0800

    angular2 test.

---
+++ src/static/app/components/home/home.js
@@ -0,0 +1,15 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'home',
+  templateUrl: '/app/components/home/home.html',
+  styleUrls: [],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class Home {
+
+  constructor() { }
+
+}
