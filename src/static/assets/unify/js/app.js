1a3f7239f6665b39441cff4b21e44fee98fe3a58
Jay.M.Hu <hm910705@163.com>
2016-02-04 22:40:23 +0800

    Add resource files, remove extra functions.

--- src/static/assets/unify/js/app.js
+++ src/static/assets/unify/js/app.js
@@ -7,176 +7,125 @@
 */
 
 var App = function () {
-    //Fixed Header
-    function handleHeader() {
-         jQuery(window).scroll(function() {
-            if (jQuery(window).scrollTop()>100){
-                jQuery(".header-fixed .header-sticky").addClass("header-fixed-shrink");
-            }
-            else {
-                jQuery(".header-fixed .header-sticky").removeClass("header-fixed-shrink");
-            }
+  //Fixed Header
+  function handleHeader() {
+    jQuery(window).scroll(function () {
+      if (jQuery(window).scrollTop() > 100) {
+        jQuery(".header-fixed .header-sticky").addClass("header-fixed-shrink");
+      }
+      else {
+        jQuery(".header-fixed .header-sticky").removeClass("header-fixed-shrink");
+      }
+    });
+  }
+
+  //Header Mega Menu
+  function handleMegaMenu() {
+    jQuery(document).on('click', '.mega-menu .dropdown-menu', function (e) {
+      e.stopPropagation();
+    })
+  }
+
+  //Search Box (Header)
+  function handleSearch() {
+    jQuery('.search').click(function () {
+      if (jQuery('.search-btn').hasClass('fa-search')) {
+        jQuery('.search-open').fadeIn(500);
+        jQuery('.search-btn').removeClass('fa-search');
+        jQuery('.search-btn').addClass('fa-times');
+      } else {
+        jQuery('.search-open').fadeOut(500);
+        jQuery('.search-btn').addClass('fa-search');
+        jQuery('.search-btn').removeClass('fa-times');
+      }
+    });
+  }
+
+  //Search Box v1 (Header v5)
+  function handleSearchV1() {
+    jQuery('.header-v5 .search-button').click(function () {
+      jQuery('.header-v5 .search-open').slideDown();
+    });
+
+    jQuery('.header-v5 .search-close').click(function () {
+      jQuery('.header-v5 .search-open').slideUp();
+    });
+
+    jQuery(window).scroll(function () {
+      if (jQuery(this).scrollTop() > 1) jQuery('.header-v5 .search-open').fadeOut('fast');
+    });
+  }
+
+  //Sidebar Navigation Toggle
+  function handleToggle() {
+    jQuery('.list-toggle').on('click', function () {
+      jQuery(this).toggleClass('active');
+    });
+  }
+
+  //Equal Height Columns    
+  function handleEqualHeightColumns() {
+    var EqualHeightColumns = function () {
+      $(".equal-height-columns").each(function () {
+        heights = [];
+        $(".equal-height-column", this).each(function () {
+          $(this).removeAttr("style");
+          heights.push($(this).height()); // write column's heights to the array
         });
+        $(".equal-height-column", this).height(Math.max.apply(Math, heights)); //find and set max
+      });
     }
 
-    //Header Mega Menu
-    function handleMegaMenu() {
-        jQuery(document).on('click', '.mega-menu .dropdown-menu', function(e) {
-            e.stopPropagation();
-        })
+    EqualHeightColumns();
+    $(window).resize(function () {
+      EqualHeightColumns();
+    });
+    $(window).load(function () {
+      EqualHeightColumns("img.equal-height-column");
+    });
+  }    
+
+  //Hover Selector
+  function handleHoverSelector() {
+    $('.hoverSelector').on('hover', function (e) {
+      $('.hoverSelectorBlock', this).toggleClass('show');
+      e.stopPropagation();
+    });
+  }    
+
+  //Bootstrap Tooltips and Popovers
+  function handleBootstrap() {
+    /*Bootstrap Carousel*/
+    jQuery('.carousel').carousel({
+      interval: 15000,
+      pause: 'hover'
+    });
+
+    /*Tooltips*/
+    jQuery('.tooltips').tooltip();
+    jQuery('.tooltips-show').tooltip('show');
+    jQuery('.tooltips-hide').tooltip('hide');
+    jQuery('.tooltips-toggle').tooltip('toggle');
+    jQuery('.tooltips-destroy').tooltip('destroy');       
+
+    /*Popovers*/
+    jQuery('.popovers').popover();
+    jQuery('.popovers-show').popover('show');
+    jQuery('.popovers-hide').popover('hide');
+    jQuery('.popovers-toggle').popover('toggle');
+    jQuery('.popovers-destroy').popover('destroy');
+  }
+
+  return {
+    init: function () {
+      handleBootstrap();
+      handleSearch();
+      handleSearchV1();
+      handleToggle();
+      handleHeader();
+      handleMegaMenu();
+      handleHoverSelector();
+      handleEqualHeightColumns();
     }
-
-    //Search Box (Header)
-    function handleSearch() {    
-        jQuery('.search').click(function () {
-            if(jQuery('.search-btn').hasClass('fa-search')){
-                jQuery('.search-open').fadeIn(500);
-                jQuery('.search-btn').removeClass('fa-search');
-                jQuery('.search-btn').addClass('fa-times');
-            } else {
-                jQuery('.search-open').fadeOut(500);
-                jQuery('.search-btn').addClass('fa-search');
-                jQuery('.search-btn').removeClass('fa-times');
-            }   
-        }); 
-    }
-
-    //Search Box v1 (Header v5)
-    function handleSearchV1() {
-        jQuery('.header-v5 .search-button').click(function () {
-            jQuery('.header-v5 .search-open').slideDown();
-        });
-
-        jQuery('.header-v5 .search-close').click(function () {
-            jQuery('.header-v5 .search-open').slideUp();
-        });
-
-        jQuery(window).scroll(function(){
-          if(jQuery(this).scrollTop() > 1) jQuery('.header-v5 .search-open').fadeOut('fast');
-        });
-    }
-
-    //Sidebar Navigation Toggle
-    function handleToggle() {
-        jQuery('.list-toggle').on('click', function() {
-            jQuery(this).toggleClass('active');
-        });
-
-        /*
-        jQuery('#serviceList').on('shown.bs.collapse'), function() {
-            jQuery(".servicedrop").addClass('glyphicon-chevron-up').removeClass('glyphicon-chevron-down');
-        }
-
-        jQuery('#serviceList').on('hidden.bs.collapse'), function() {
-            jQuery(".servicedrop").addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
-        }
-        */
-    }
-
-    //Equal Height Columns    
-    function handleEqualHeightColumns() {
-        var EqualHeightColumns = function () {            
-            $(".equal-height-columns").each(function() {
-                heights = [];              
-                $(".equal-height-column", this).each(function() {
-                    $(this).removeAttr("style");
-                    heights.push($(this).height()); // write column's heights to the array
-                });
-                $(".equal-height-column", this).height(Math.max.apply(Math, heights)); //find and set max
-            });
-        }
-
-        EqualHeightColumns();        
-        $(window).resize(function() {            
-            EqualHeightColumns();
-        });
-        $(window).load(function() {
-            EqualHeightColumns("img.equal-height-column");
-        });
-    }    
-
-    //Hover Selector
-    function handleHoverSelector() {
-        $('.hoverSelector').on('hover', function(e) {        
-            $('.hoverSelectorBlock', this).toggleClass('show');
-            e.stopPropagation();            
-        });
-    }    
-
-    //Bootstrap Tooltips and Popovers
-    function handleBootstrap() {
-        /*Bootstrap Carousel*/
-        jQuery('.carousel').carousel({
-            interval: 15000,
-            pause: 'hover'
-        });
-
-        /*Tooltips*/
-        jQuery('.tooltips').tooltip();
-        jQuery('.tooltips-show').tooltip('show');      
-        jQuery('.tooltips-hide').tooltip('hide');       
-        jQuery('.tooltips-toggle').tooltip('toggle');       
-        jQuery('.tooltips-destroy').tooltip('destroy');       
-
-        /*Popovers*/
-        jQuery('.popovers').popover();
-        jQuery('.popovers-show').popover('show');
-        jQuery('.popovers-hide').popover('hide');
-        jQuery('.popovers-toggle').popover('toggle');
-        jQuery('.popovers-destroy').popover('destroy');
-    }
-
-    return {
-        init: function () {
-            handleBootstrap();
-            handleSearch();
-            handleSearchV1();
-            handleToggle();
-            handleHeader();
-            handleMegaMenu();
-            handleHoverSelector();
-            handleEqualHeightColumns();
-        },
-
-        //Scroll Bar 
-        initScrollBar: function () {
-            jQuery('.mCustomScrollbar').mCustomScrollbar({
-                theme:"minimal",
-                scrollInertia: 300,
-                scrollEasing: "linear"
-            });
-        },
-
-        //Parallax Backgrounds
-        initParallaxBg: function () {
-             jQuery(window).load(function() {
-                jQuery('.parallaxBg').parallax("50%", 0.2);
-                jQuery('.parallaxBg1').parallax("50%", 0.4);
-            });
-        },
-
-        //Animate Dropdown
-        initAnimateDropdown: function () {
-            function MenuMode() {
-                jQuery('.dropdown').on('show.bs.dropdown', function(e){
-                    jQuery(this).find('.dropdown-menu').first().stop(true, true).slideDown();
-                });
-                jQuery('.dropdown').on('hide.bs.dropdown', function(e){
-                    jQuery(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-                });
-            }
-         
-            jQuery(window).resize(function(){
-                if (jQuery(window).width() > 768) {
-                    MenuMode();
-                }
-            });
-
-            if (jQuery(window).width() > 768) {
-                MenuMode();
-            }
-        },
-
-    };
-
-}();
+  };
+} ();
