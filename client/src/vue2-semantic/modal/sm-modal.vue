9f16cd8d1c447174fe762b13b00f9ce7a9c05198
Jay.M.Hu <hm910705@163.com>
2017-01-25 14:02:38 +0800

    optimized sm-modal.

--- client/src/vue2-semantic/modal/sm-modal.vue
+++ client/src/vue2-semantic/modal/sm-modal.vue
@@ -1,21 +1,31 @@
+<style lang="stylus">
+  .sm-modal{
+    overflow: auto;
+  }
+</style>
 <template>
-  <div class="ui modal sm-modal">
-    <i class="close icon"></i>
-    <div class="header">
-      <slot name="modal-header"></slot>
-      <template v-if="!$slots['modal-header']">
-        {{header}}
-      </template>
-    </div>
-    <div class="content">
-      <slot></slot>
-    </div>
-    <div class="actions">
-      <slot name="modal-actions"></slot>
-      <template v-if="!$slots['modal-actions']">
-        <div class="ui button cancel">Cancel</div>
-        <div class="ui button ok">OK</div>
-      </template>
+  <div class="ui dimmer modals page transition fade sm-modal" :class="{active: value}">
+    <div class="ui standard modal transition scrolling fade" :class="[animate, {active: value, in: value, out: !value || animating}]">
+      <i class="close icon" v-if="closable" @click="onCancelClick"></i>
+      <div class="header">
+        <slot name="modal-header"></slot>
+        <template v-if="!$slots['modal-header']">
+          {{header}}
+        </template>
+      </div>
+      <div class="content">
+        <slot></slot>
+      </div>
+      <div class="actions">
+        <slot name="modal-actions"></slot>
+        <template v-if="!$slots['modal-actions']">
+          <div class="ui button deny cancel" @click.prevent="onCancelClick()">{{cancelText}}</div>
+          <div class="ui positive right labeled icon button" @click.prevent="onOkClick()">
+            {{okText}}
+            <i class="checkmark icon"></i>
+          </div>
+        </template>
+      </div>
     </div>
   </div>
 </template>
@@ -27,43 +37,25 @@
       header: { type: String, default: '' },
       okText: { type: String, default: 'OK' },
       cancelText: { type: String, default: 'Cancel' },
-      closable: { type: Boolean, default: false }
+      closable: { type: Boolean, default: true },
+      animate: { type: String, default: 'horizontal flip' }
     },
     data() {
       return {
-        $modal: null
+        animating: false
       };
     },
-    mounted() {
-      let self = this;
-      this.$modal = $(this.$el).modal({
-        // context: $(this.$el.parent),
-        closable: this.closable,
-        onVisible() {
-          self.$emit('input', true);
-        },
-        onHidden() {
-          self.$emit('input', false);
-        },
-        onApprove() {
-          self.$emit('ok');
-          return false;
-        },
-        onDeny() {
-          self.$emit('input', false);
-          self.$emit('cancel');
-        }
-      });
-      this.setModalStatus();
-    },
-    watch: {
-      value(newVal) {
-        this.setModalStatus();
-      }
-    },
     methods: {
-      setModalStatus() {
-        this.$modal.modal(this.value ? 'show' : 'hide');
+      onCancelClick() {
+        this.animating = true;
+        setTimeout(() => {
+          this.$emit('input', false);
+          this.animating = false;
+        }, 400);
+        this.$emit('cancel');
+      },
+      onOkClick() {
+        this.$emit('ok');
       }
     }
   };
